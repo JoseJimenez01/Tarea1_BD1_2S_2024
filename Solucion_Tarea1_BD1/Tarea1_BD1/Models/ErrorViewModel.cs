17c8c87bc0cfc92330ef73018a4412d34d904589
JoseJimenez01 <jg3633110@gmail.com>
2024-08-07 18:50:34 -0600

    Creación plantilla
    Creada la plantilla del proyecto justo para empezar a trabajar.

---
+++ Solucion_Tarea1_BD1/Tarea1_BD1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea1_BD1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

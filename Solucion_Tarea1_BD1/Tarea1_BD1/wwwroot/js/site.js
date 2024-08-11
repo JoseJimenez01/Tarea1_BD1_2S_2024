cab2b225bb6cc6d104d1c9df9ef257e85cc40738
JoseJimenez01 <jg3633110@gmail.com>
2024-08-11 16:22:22 -0600

    Validaciónes del formulario
    Se valida correctamente las entradas del formulario, se hizo el SP para agregar empleados y se empezó el popup para hacer avisos al usuario.

--- Solucion_Tarea1_BD1/Tarea1_BD1/wwwroot/js/site.js
+++ Solucion_Tarea1_BD1/Tarea1_BD1/wwwroot/js/site.js
@@ -12,10 +12,60 @@ function ObtenerColorDeFondo() {
 function validarFormulario() {
     var nombre = document.getElementById("entNom").value;
     var salario = document.getElementById("entSal").value;
-    //console.log(nombre);
-    //console.log(salario);
-    //alert(nombre + salario);
+
+    let expresionRegularNombre = /[A-Za-z\ \-\xC1\xC9\xCD\xD3\xDA\xDC\xE1\xE9\xED\xF3\xFA\xFC\xD1\xF1]+/g;
+    let expresionRegularSalario = /[\d]+\.{1}[\d]{2}/g;
+
     if (nombre == "") {
-        alert("Nombre obligatorio");
+        //alert("Debe digitar el nombre.");
+
+    //La doble validación es necesaria
+    } else if (expresionRegularNombre.test(nombre) === false || !(nombre.match(expresionRegularNombre) == nombre)) {
+        alert("El nombre solo puede incluir letras, espacios o guiones.");
+    } else if (salario == "") {
+        alert("Debe digitar el salario.");
+        //La doble validación es necesaria
+    } else if (expresionRegularSalario.test(salario) === false || !(salario.match(expresionRegularSalario) == salario)) {
+        alert("El salario solo puede usar el punto como separador decimal y dos decimales obligatorios.");
     }
 }
+
+function cerrarDivAvisos() {
+
+}
+
+/*
+    Desde aqui el JS para el popup de avisos
+*/
+
+const abrirPopup = document.getElementById('btnSubmit');
+const cerrarPopup = document.getElementById('btnCerrarPopup');
+const contenedorPopup = document.getElementById('elPopup');
+const taparContenido = document.getElementById('overlay');
+
+
+// Funciones para abrir y cerrar el popup
+function showPopup() {
+    taparContenido.style.display = 'block';
+    contenedorPopup.style.display = 'block';
+}
+
+function hidePopup() {
+    taparContenido.style.display = 'none';
+    contenedorPopup.style.display = 'none';
+}
+
+// Event listeners para abrir y cerrar el popup
+abrirPopup.addEventListener('click', showPopup);
+cerrarPopup.addEventListener('click', hidePopup);
+
+document.addEventListener('click', function (event) {
+    // Valida si el clic sucedio en el contenedor overlay
+    if (taparContenido.contains(event.target)) {
+        hidePopup();
+    }
+});
+
+/*
+    Hasta aqui el JS para el popup de avisos
+*/
